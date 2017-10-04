7d9e96ff835b050d691dae6cdb0f61dc12579244
nikvs84 <nikvs84@rambler.ru>
2017-10-04 16:09:17 +0300

    Add Jackson converter, RemindController (@Controller -> @RestController).

--- src/main/java/com/nikvs84/remindme/server/controller/ReminderController.java
+++ src/main/java/com/nikvs84/remindme/server/controller/ReminderController.java
@@ -1,18 +1,28 @@
 package com.nikvs84.remindme.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.nikvs84.remindme.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/reminder")
 public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap modelMap) {
-        return "My reminder";
+    public Remind getReminder() {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1l);
+        remind.setRemindDate(new Date());
+        remind.setTitle("My first remind");
+        return remind;
     }
 }
